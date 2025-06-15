3e772ccf29577ea20884ccd680f8988d7cb5ef04
Dhruv <buffering.bit@gmail.com>
2025-06-15 18:37:18 +0530

    UI updated with Daisy UI.

--- src/routes/login-01/+page.svelte
+++ src/routes/login-01/+page.svelte
@@ -18,12 +18,12 @@
 	<LoginForm />
 </div> -->
 
-<div class = "hero min-h-screen bg-base-300">
+<!-- <div class = "hero min-h-screen bg-base-300">
 	<div class="hero-content w-[800px]">
 		<div class="flex flex-col">
 			<p>Create an account or login below!</p>
 			<!-- Supabase Auth Ui -->
-			<Auth 
+			<!-- <Auth 
 				supabaseClient = {supabase}
 				theme = "dark"
 				appearance={{
@@ -36,4 +36,32 @@
 			/>
 		</div>
 	</div>
+</div> -->
+
+<div class="hero bg-base-200 min-h-screen">
+  <div class="hero-content flex-col lg:flex-row-reverse">
+	<div class="text-center lg:text-left">
+	  <h1 class="text-5xl font-bold">Login now!</h1>
+	  <p class="py-6">
+		Login Now, to access mystery box! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi repellendus ullam tempora, ratione veritatis, minima porro molestiae aliquam, pariatur sunt vero cupiditate? Consequuntur suscipit ea, inventore, aut cum doloremque, ipsum obcaecati dignissimos voluptatem consectetur quibusdam?
+	  </p>
+	</div>
+	<Auth 
+				supabaseClient = {supabase}
+				theme = "dark"
+				appearance={{
+					theme: ThemeSupa,
+					style: {
+						input: "width: 400px; height: 50px; color: #fff"
+					}
+				}}
+			
+			/>
+  </div>
 </div>
+
+<style>
+	:global(body) {
+		color: white;
+	}
+</style>
--- src/routes/mystery/+page.svelte
+++ src/routes/mystery/+page.svelte
@@ -13,4 +13,35 @@
 		goto("/login-01");
 	}
 </script>
-<h1>Mystery unboxed.</h1>
+<div class="mockup-browser border-base-300 border w-full" data-theme="dark">
+  <div class="mockup-browser-toolbar">
+    <div class="input">https://daisyui.com</div>
+  </div>
+  <div class="grid place-content-center border-t border-base-300 h-80">Hello, You have achieved this mystery box.</div>
+
+  <div class="hero bg-base-200 min-h-screen">
+  <div class="hero-content flex-col lg:flex-row-reverse">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+      alt="DaisyUI"
+      class="max-w-sm rounded-lg shadow-2xl"
+    />
+    <div>
+      <h1 class="text-5xl font-bold">Box Office News!</h1>
+      <p class="py-6">
+        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+        quasi. In deleniti eaque aut repudiandae et a id nisi.
+      </p>
+      <button class="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
+</div>
+
+
+
+<style>
+	:global(body){
+		color: royalblue;
+	}
+</style>
